7a15adbf3db20d5c7827e90d0fe76f61c5633026
Narutaka <narutaka@minamide.org>
2021-04-22 10:19:44 +0900

    Update AddHash.pl

--- AddHash.pl
+++ AddHash.pl
@@ -1,17 +1,21 @@
+#
+# ハッシュ結合(同じKeyは配列に)
+#
 
-	use strict;
-	use warnings;
-	use Encode;
-	use Data::Dumper;
+use utf8;
+use strict;
+use warnings;
+use Encode;
+use Data::Dumper;
 
-	my %hash1 = ( aaa=> 1111, bbb=> 2222, ccc=> 3333, );
-	my %hash2 = ( bbb=> 4444, );
-	my %hash3 = ( eee=> 'eeee', );
+my %hash1 = ( aaa=> 1111, bbb=> 2222, ccc=> 3333, );
+my %hash2 = ( bbb=> 4444, );
+my %hash3 = ( eee=> 'eeee', );
 
-	my %addhash1= &myHasuAdd(\%hash1, \%hash2);
-	my %addhash2= &myHasuAdd(\%addhash1, \%hash3);
+my %addhash1= &myHasuAdd(\%hash1, \%hash2);
+my %addhash2= &myHasuAdd(\%addhash1, \%hash3);
 
-	print Dumper \%addhash2;
+print Dumper \%addhash2;
 
 sub myHasuAdd
 {
