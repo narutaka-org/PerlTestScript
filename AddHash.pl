90a8eb9ce761565836265016aa9ee85a37bc1bb0
Narutaka <narutaka@minamide.org>
2021-04-22 10:30:21 +0900

    Update AddHash.pl

--- AddHash.pl
+++ AddHash.pl
@@ -5,7 +5,7 @@
 use utf8;
 use strict;
 use warnings;
-use Encode;
+#use Encode;
 use Data::Dumper;
 
 my %hash1 = ( aaa=> 1111, bbb=> 2222, ccc=> 3333, );
@@ -21,19 +21,15 @@ sub myHasuAdd
 {
 	my %org = %{$_[0]};
 	my %add = %{$_[1]};
-	foreach my $key(keys(%add))
-	{
-		if( $org{$key} eq "" )
-		{
+	foreach my $key(keys(%add)){
+		if( !defined($org{$key}) ){ $org{$key} = ""; }
+		if( $org{$key} eq "" ){
 			$org{$key} = $add{$key};
 		}else{
-			if( ref($org{$key}) eq 'ARRAY')
-			{
+			if( ref($org{$key}) eq 'ARRAY'){
 				push(@{$org{$key}},$add{$key});
 			}else{
-				my @list;
-				push(@list,$org{$key});
-				push(@list,$add{$key});
+				my @list = ($org{$key},$add{$key});
 				$org{$key} = \@list;
 			}
 		}
